d2dc975dc579fe647188ae4de778e26e0fce9165
Kevvinn <kevinhu229@gmail.com>
2021-03-09 22:44:49 -0800

    update readme and add navigation for journals and game

--- src/main/java/com/example/demo/config/CustomizeAuthenticationSuccessHandler.java
+++ src/main/java/com/example/demo/config/CustomizeAuthenticationSuccessHandler.java
@@ -20,7 +20,7 @@ public class CustomizeAuthenticationSuccessHandler implements AuthenticationSucc
 
         for (GrantedAuthority auth : authentication.getAuthorities()) {
             if ("ADMIN".equals(auth.getAuthority())) {
-                response.sendRedirect("/dashboard");
+                response.sendRedirect("/uno");
             }
         }
     }
--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -41,6 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/uno").permitAll()
                 .antMatchers("/").permitAll()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/signup").permitAll()
