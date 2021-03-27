de9e988ccba014847e138ab0949493cae3ede37f
Alex Hu <56702257+AlexH04@users.noreply.github.com>
2021-03-26 22:08:19 -0700

    Create FactorialModel.java

---
+++ minilabs/FactorialModel.java
@@ -0,0 +1,19 @@
+package minilabs;
+import minilabs.FactorialController.java;
+
+public class FactorialModel{
+    public static void main (String[] args) {
+        //only algo has been implemented for now
+    }
+
+    public static int fact(int n) {
+        if (n >= 0) {
+            if (n == 0) {return 1;}
+            else {
+                return n * fact(n - 1);
+            }
+        } else {
+            return -1; //to indicate that n is negative
+        }
+    }
+}
