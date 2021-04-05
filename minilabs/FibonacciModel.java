bd95aeeff016c5c0c7bd7c874f8e980e635ab0b6
andrewzpu <andrewzpu@gmail.com>
2021-04-05 09:06:40 -0700

    Fibonacci Changes

--- minilabs/FibonacciModel.java
+++ minilabs/FibonacciModel.java
@@ -3,28 +3,28 @@ import java.util.ArrayList;
 
 public class FibonacciModel {
 
-    ArrayList<Integer> list;
+    ArrayList<Integer> list = new ArrayList<Integer>();
     int size;
 
     public FibonacciModel(int s) {
         size = s;
         list.add(0);
         list.add(1);
-        Factorial(size, 0, 1);
+        Fibonacci(size, 0, 1);
     }
 
     //size must be at least 2
-    public void Factorial(int size, int a, int b) {
+    public void Fibonacci(int size, int a, int b) {
         if (size == 2) {return;}
         list.add(a + b);
-        Factorial(size - 1, b, a + b);
+        Fibonacci(size - 1, b, a + b);
     }
 
     public void print() {
         System.out.println("Sequence Size: " + size);
         System.out.print("Sequence: " + list.get(0));
         for(int i = 1; i < size; i++) {
-            System.out.print(", " + list.get(0));
+            System.out.print(", " + list.get(i));
         }
     }
 
