eb4490eee771a468cf9c4c16ae9392bb0f0101b2
atharvakudrimoti <atharva.kudrimoti@gmail.com>
2021-04-13 01:20:11 -0700

    cakeshop inheritance lab

---
+++ src/main/java/minilabz/CakeShop.java
@@ -0,0 +1,54 @@
+package minilabz;
+
+import consoleUI.ConsoleMethods;
+
+public class CakeShop extends Generic {
+        public enum KeyType {title, caketype, frosting}
+        public static minilabz.CakeShop.KeyType key = minilabz.CakeShop.KeyType.title;
+        public String caketype;
+        public boolean frosting;
+
+        CakeShop(String caketype, boolean frosting)
+        {
+            this.setType("Cake");
+            this.caketype = caketype;
+            this.frosting = frosting;
+        }
+
+        public String toString() {
+            String output="";
+            switch (key) {
+                case caketype:
+                    output += this.caketype;
+                    break;
+                case frosting:
+                    output += this.frosting;
+                    break;
+                case title:
+                default:
+                    output = super.getType() + ": " + this.caketype + ", " + this.frosting;
+            }
+
+            return output;
+        }
+
+        public static Generic[] CakeShopData () {
+            return new Generic[]{
+                    new minilabz.CakeShop("Boston Cream Cake", true),
+                    new minilabz.CakeShop("Boston Cream Cake", false),
+                    new minilabz.CakeShop("Ice Cream Cake", true),
+                    new minilabz.CakeShop("Ice Cream Cake", false),
+                    new minilabz.CakeShop("Strawberry Shortcake", true),
+                    new minilabz.CakeShop("Strawberry Shortcake", false),
+                    new minilabz.CakeShop("Black Forest Cake", true),
+                    new minilabz.CakeShop("Black Forest Cake", false),
+            };
+        }
+
+        public static void main(String[] args)
+        {
+            Generic[] CakeShop = CakeShopData();
+            for(Generic c:  CakeShop)
+                ConsoleMethods.println(c);
+        }
+}
